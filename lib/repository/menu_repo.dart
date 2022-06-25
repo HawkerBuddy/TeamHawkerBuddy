a44838e10eeff2eaed19ca72a190bc8089cb8184
SFCoding123 <shaofeng1234567@gmail.com>
2022-06-26 00:22:45 +0800

    UI Updates

--- lib/repository/menu_repo.dart
+++ lib/repository/menu_repo.dart
@@ -5,7 +5,7 @@ import 'package:hawker_buddy/data/api/api_client.dart';
 class MenuRepo extends GetxService {
   final ApiClient apiClient;
   MenuRepo({required this.apiClient});
-
+//
 
   /*
   Future<Response> getMenuList() async {
