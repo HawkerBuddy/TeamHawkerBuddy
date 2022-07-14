5f2698073987f281319c239a1b7083a1ac82d043
Teo Weide <teoweide1999@gmail.com>
2022-07-14 21:46:09 +0800

    IDfiles

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -8,7 +8,7 @@ import 'package:hawker_buddy/pages/home/newHome.dart';
 import 'package:hawker_buddy/widgets/small_text.dart';
 import 'package:hawker_buddy/widgets/unique_text.dart';
 
-import '../../data_controller.dart';
+//import '../../data_controller.dart';
 import '../../routes/router_helper.dart';
 import '../../utils/colors.dart';
 import '../../utils/dimensions.dart';
@@ -22,30 +22,26 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
-
   int count = 0;
   double price = 2.5;
   void _increase() {
-    setState(()
-    { count++;
+    setState(() {
+      count++;
     });
-
   }
+
   void _decrease() {
-    if(count<1) {
+    if (count < 1) {
       return;
     }
 
     setState(() {
       count--;
     });
-
   }
 
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
       backgroundColor: Colors.white,
       body: Stack(
@@ -54,134 +50,150 @@ class _CartPageState extends State<CartPage> {
               top: Dimensions.height45,
               left: Dimensions.width20,
               right: Dimensions.width20,
-
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   GestureDetector(
                       onTap: () {
-                        if(Get.previousRoute == RouterHelper.initial) {
+                        if (Get.previousRoute == RouterHelper.initial) {
                           //Get.to(HomePage());
-                        }
-                        else {
+                        } else {
                           Get.back();
                         }
                       },
-                      child: AppIcons(icon: Icons.arrow_back_outlined,size:50)),
+                      child:
+                          AppIcons(icon: Icons.arrow_back_outlined, size: 50)),
                   //View Digital Menu
                   GestureDetector(
                       onTap: () {
                         Get.toNamed(RouterHelper.initial);
                       },
-                      child: AppIcons(icon: Icons.home,size:50,)),
-                  AppIcons(icon: Icons.shopping_cart,size:50),
+                      child: AppIcons(
+                        icon: Icons.home,
+                        size: 50,
+                      )),
+                  AppIcons(icon: Icons.shopping_cart, size: 50),
                 ],
               )),
           Positioned(
-              top:Dimensions.height20 * 5,
+              top: Dimensions.height20 * 5,
               left: Dimensions.width20,
               right: Dimensions.width20,
               bottom: 0,
-              child:
-          Container(
-            //color: Colors.blue,
-            margin: EdgeInsets.only(top: Dimensions.height10),
-            child: MediaQuery.removePadding(
-              context: context,
-              removeTop: true,
-              child: ListView.builder(
-                  itemCount: 1,
-                  itemBuilder: (_, index){
-                    return Container(
-                      height: 100,
-                      width: double.maxFinite,
-
-                      child: Row(
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              //DataController.addDoc("user", DataController.user);
-                              //DataController.read("user");
-                              DataController.create();
-
-                            },
-                            child: Container(
-                              width: Dimensions.height20*5,
-                              height: Dimensions.height20*5,
-                              margin: EdgeInsets.only(bottom: Dimensions.height10),
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: AssetImage(
-                                    "assets/images/mala.png"
-                                  )
+              child: Container(
+                //color: Colors.blue,
+                margin: EdgeInsets.only(top: Dimensions.height10),
+                child: MediaQuery.removePadding(
+                  context: context,
+                  removeTop: true,
+                  child: ListView.builder(
+                      itemCount: 1,
+                      itemBuilder: (_, index) {
+                        return Container(
+                          height: 100,
+                          width: double.maxFinite,
+                          child: Row(
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  //DataController.addDoc("user", DataController.user);
+                                  //DataController.read("user");
+                                  //DataController.create();
+                                },
+                                child: Container(
+                                  width: Dimensions.height20 * 5,
+                                  height: Dimensions.height20 * 5,
+                                  margin: EdgeInsets.only(
+                                      bottom: Dimensions.height10),
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        fit: BoxFit.cover,
+                                        image: AssetImage(
+                                            "assets/images/mala.png")),
+                                    borderRadius: BorderRadius.circular(
+                                        Dimensions.radius20),
+                                    color: Colors.white,
+                                  ),
                                 ),
-                                borderRadius: BorderRadius.circular(Dimensions.radius20),
-                                color: Colors.white,
                               ),
-                            ),
+                              SizedBox(width: Dimensions.width10),
+                              Expanded(
+                                  child: Container(
+                                height: Dimensions.height20 * 5,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    uniqueText(text: "Mala"),
+                                    miniText(text: "Super"),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        uniqueText(
+                                          text:
+                                              "\$" + (price * count).toString(),
+                                          color: Colors.redAccent,
+                                          size: Dimensions.font15,
+                                        ),
+                                        Container(
+                                          height: Dimensions.height50,
+                                          padding: EdgeInsets.only(
+                                              top: Dimensions.width10,
+                                              left: Dimensions.width20,
+                                              bottom: Dimensions.width20,
+                                              right: Dimensions.width10),
+                                          decoration: BoxDecoration(
+                                              //color: AppColors.iconshopopen,
+                                              borderRadius: BorderRadius.only(
+                                                  topLeft: Radius.circular(
+                                                      Dimensions.radius20 * 2),
+                                                  topRight: Radius.circular(
+                                                      Dimensions.radius20 *
+                                                          2))),
+                                          child: Row(
+                                            children: [
+                                              GestureDetector(
+                                                  onTap: () {
+                                                    _decrease();
+                                                  },
+                                                  child: Container(
+                                                      child: Icon(
+                                                    Icons.remove,
+                                                    color: Colors.grey,
+                                                  ))),
+                                              SizedBox(
+                                                  width:
+                                                      Dimensions.width10 / 2),
+                                              uniqueText(
+                                                  text: count.toString()),
+                                              SizedBox(
+                                                  width:
+                                                      Dimensions.width10 / 2),
+                                              GestureDetector(
+                                                  onTap: () {
+                                                    _increase();
+                                                  },
+                                                  child: Container(
+                                                      child: Icon(
+                                                    Icons.add,
+                                                    color: Colors.grey,
+                                                  )))
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ))
+                            ],
                           ),
-                          SizedBox(width: Dimensions.width10),
-                          Expanded(child: Container(
-                            height: Dimensions.height20*5,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                uniqueText(text: "Mala"),
-                                miniText(text: "Super"),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                 children: [
-                                   uniqueText(text: "\$" + (price * count).toString(),color: Colors.redAccent,size: Dimensions.font15,),
-                                   Container(
-
-                                     height: Dimensions.height50,
-                                     padding: EdgeInsets.only(top: Dimensions.width10, left: Dimensions.width20, bottom: Dimensions.width20, right: Dimensions.width10),
-                                     decoration: BoxDecoration(
-                                       //color: AppColors.iconshopopen,
-                                         borderRadius: BorderRadius.only(
-                                             topLeft: Radius.circular(Dimensions.radius20*2),
-                                             topRight: Radius.circular(Dimensions.radius20*2)
-                                         )
-                                     ),
-
-                                     child:Row(
-                                       children: [
-                                                 GestureDetector(
-                                                     onTap:(){
-                                                       _decrease();
-                                                     },
-                                                     child: Container(child: Icon(Icons.remove, color: Colors.grey,))),
-                                                 SizedBox(width: Dimensions.width10/2),
-                                                 uniqueText(text: count.toString()),
-                                                 SizedBox(width: Dimensions.width10/2),
-                                                 GestureDetector(
-                                                     onTap: (){
-                                                       _increase();
-                                                     },
-                                                     child: Container(child: Icon(Icons.add,color: Colors.grey,)))
-
-
-                                       ],
-                                     ) ,
-                                   ),
-
-
-                                 ],
-                                )
-
-                              ],
-                            ),
-                          ))
-                        ],
-                      ),
-
-                    );
-                  }),
-            ),
-
-          ))
+                        );
+                      }),
+                ),
+              ))
         ],
       ),
     );
