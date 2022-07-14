2c83648b5aade05916dd50c1378551dd1bd21482
SFCoding123 <shaofeng1234567@gmail.com>
2022-07-14 23:05:16 +0800

    14 July v2

---
+++ lib/data/cart_data.dart
@@ -0,0 +1,38 @@
+
+
+class CartData {
+
+  int? quantity;
+  final double? price;
+  double? totalPrice;
+  String? foodID;
+
+  CartData({this.price, this.foodID});
+
+
+  setQuantity(int quantity) {
+    this.quantity = quantity;
+    totalPrice = quantity * price!;
+  }
+
+  int? getQuantity(){
+    return quantity;
+  }
+
+  double? gettotalPrice(){
+    return totalPrice;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
