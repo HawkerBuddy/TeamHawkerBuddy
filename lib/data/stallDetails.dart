d3d905d036338a2a0b11365e6d204bb87e727e46
SFCoding123 <shaofeng1234567@gmail.com>
2022-07-20 00:40:39 +0800

    20 July

--- lib/data/stallDetails.dart
+++ lib/data/stallDetails.dart
@@ -73,33 +73,97 @@ class textStallYIH {
     return saveName;
   }
 
-  Future<List<String>> _getFoodName(String stallName) async {
+  Future<List<String>> _getFoodName(String stallID, String output) async {
     List<String> saveName = [];
     var data = await FirebaseFirestore.instance
         .collection('Canteen')
         .doc('PGP')
         .collection('Stalls')
-        .doc(stallName)
+        .doc(stallID)
         .collection('menu')
         .get();
    //input take in List<String> then u output List<list<String>>>;
-    saveName = List.from(data.docs.map((doc) => doc.get("foodName")));
+    saveName = List.from(data.docs.map((doc) => doc.get(output)));
     return saveName;
   }
 
-  Future<List<List<String>>> foodName(List<String> stallName) async {
+  Future<List<List<String>>> foodName(List<String> stallID) async {
     int row = 34;
-
     var food  = List.generate(10, (i) => List.filled(10, "", growable: true), growable: true);
 
-    for (int i = 0; i < stallName.length; i++) {
-      List<String> noOfFood = await _getFoodName(stallName[i]);
+    for (int i = 0; i < stallID.length; i++) {
+      List<String> noOfFood = await _getFoodName(stallID[i], "foodName");
       //print(noOfFood.length);
       for (int j = 0; j < noOfFood.length; j++) {
         //print(j+10000);
         print(noOfFood[j]);
         food[i][j] = noOfFood[j];
        //print(food[i][j]);
+      }
+    }
+    return food;
+  }
+  Future<List<List<String>>> foodImgUrl(List<String> stallID) async {
+
+    var food  = List.generate(10, (i) => List.filled(10, "", growable: true), growable: true);
+
+    for (int i = 0; i < stallID.length; i++) {
+      List<String> noOfFood = await _getFoodName(stallID[i], "image");
+      //print(noOfFood.length);
+      for (int j = 0; j < noOfFood.length; j++) {
+        //print(j+10000);
+        print(noOfFood[j]);
+        food[i][j] = noOfFood[j];
+        //print(food[i][j]);
+      }
+    }
+    return food;
+  }
+  Future<List<List<String>>> foodPrice(List<String> stallID) async {
+
+    var food  = List.generate(10, (i) => List.filled(10, "", growable: true), growable: true);
+
+    for (int i = 0; i < stallID.length; i++) {
+      List<String> noOfFood = await _getFoodName(stallID[i], "price");
+      //print(noOfFood.length);
+      for (int j = 0; j < noOfFood.length; j++) {
+        //print(j+10000);
+        print(noOfFood[j]);
+        food[i][j] = noOfFood[j];
+        //print(food[i][j]);
+      }
+    }
+    return food;
+  }
+  Future<List<List<String>>> foodDes(List<String> stallID) async {
+
+    var food  = List.generate(10, (i) => List.filled(10, "", growable: true), growable: true);
+
+    for (int i = 0; i < stallID.length; i++) {
+      List<String> noOfFood = await _getFoodName(stallID[i], "description");
+      //print(noOfFood.length);
+      for (int j = 0; j < noOfFood.length; j++) {
+        //print(j+10000);
+        print(noOfFood[j]);
+        food[i][j] = noOfFood[j];
+        //print(food[i][j]);
+      }
+    }
+    return food;
+  }
+
+  Future<List<List<String>>> foodID(List<String> stallID) async {
+
+    var food  = List.generate(10, (i) => List.filled(10, "", growable: true), growable: true);
+
+    for (int i = 0; i < stallID.length; i++) {
+      List<String> noOfFood = await _getFoodName(stallID[i], "foodId");
+      //print(noOfFood.length);
+      for (int j = 0; j < noOfFood.length; j++) {
+        //print(j+10000);
+        print(noOfFood[j]);
+        food[i][j] = noOfFood[j];
+        //print(food[i][j]);
       }
     }
     return food;
